b1c64162a5c5c85126add607622e56af6e232950
William Enemali <simplewilliamz@gmail.com>
2016-10-27 16:54:33 +0100

    OOP tutorial

---
+++ OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Name name = new Name("Vardy","Jamie","Onaga");
+            Department department = new Department("Agric Science");
+            Student chisom = new Student(name,department);
+
+            Console.WriteLine(chisom.StudentName.Fullname);
+            Console.WriteLine(chisom.StudentName.Initials);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Student
+    {
+        //Properties
+        public Name StudentName { get; set; }
+        public Department StudentDepartment { get; set; }
+        public string State { get; set; }
+        public string LGA { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string MatricNumber { get; set; }
+        public string JambNumber { get; set; }
+        public string StudentType { get; set; }
+        public string CourseData { get; set; }
+
+        //Constructor
+        public Student(Name studentName, Department studentDepartment)
+        {
+            StudentName = studentName;
+            StudentDepartment = studentDepartment;
+        }
+    }
+
+    public class Name
+    {
+        //Properties
+        public string Surname { get; set; }
+        public string Firstname { get; set; }
+        public string Othername { get; set; }
+        public string Fullname { get; set; }
+        public string Initials { get; set; }
+
+        //Constructor
+        public Name(string sname, string fname, string oname ="")
+        {
+            Surname = sname.ToUpper();
+            Firstname = fname.ToUpper();
+            Othername = oname.ToUpper();
+            Fullname = Surname + " " + Firstname + " " + Othername;
+            if (string.IsNullOrEmpty(oname))
+            {
+                Initials = Surname.Substring(0,1) + "." + Firstname.Substring(0,1) ;
+
+            }
+            else
+            {
+                Initials = Surname.Substring(0, 1) + "." + Firstname.Substring(0, 1) + "." + Othername.Substring(0,1);
+
+            }
+            
+        }
+    }
+
+    public class Department
+    {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
